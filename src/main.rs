24d39d2a1dfcc4d055044cdbde63fa14ab98fb62
Theocharis Giannitsis <40761063+theoteck@users.noreply.github.com>
2020-08-02 21:16:12 +0300

    Removed some annoying printlns

--- src/main.rs
+++ src/main.rs
@@ -243,10 +243,6 @@ fn fetch_unfollowers() -> Fallible<()> {
         }
 
         if butitsthesamee_fod > 3 {
-            println!("");
-            println!("Found the same following count the past 4 times...");
-            println!("Either Instagram is bad at counting or you have a network error (probably the 1st)");
-            println!("Continuing...");
             break;
         }
     }
@@ -341,10 +337,6 @@ fn fetch_unfollowers() -> Fallible<()> {
         }
 
         if butitsthesamee_fol > 3 {
-            println!("");
-            println!("Found the same follower count the past 4 times...");
-            println!("Either Instagram is bad at counting or you have a network error (probably the 1st)");
-            println!("Continuing...");
             break;
         }
     }
